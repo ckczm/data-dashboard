8423a3c3a7b7dfcadb150536182b714bf42658bd
ckczm <lukas.zywicki@gmail.com>
2022-02-20 23:32:11 +0100

    ADD: scraping job title, salary, seniority and requirements

--- src/scraper.py
+++ src/scraper.py
@@ -34,12 +34,33 @@ from bs4 import BeautifulSoup
 
 headers = {"user-agent": "lukasz.zywicki@gmail.com"}
 content = requests.get(
-    url="https://nofluffjobs.com/pl/praca-it/big-data",
+    url="https://nofluffjobs.com/pl/job/data-architect-with-data-modeling-erwin-devire-remote-tfvhi8l3",
     headers=headers
 )
 soup = BeautifulSoup(content.text, "lxml")
-for link in soup.find_all("a"):
-    if "/pl/job" and "erwin" in link.get("href"):
-        print(link.get("href"))
-        print(link.contents[3].h3.string)
-        print(link.contents[4].span.text)
+# for link in soup.find_all("a"):
+#     if "/pl/job" and "erwin" in link.get("href"):
+#         print(link.get("href"))
+#         print(link.contents[3].h3.string)
+#         print(link.contents[4].span.text)
+print(soup.select_one(".posting-details-description").h1.text)
+print(soup.find("common-posting-salaries-list").h4.text)
+print(soup.find("common-posting-seniority").span.text)
+# print(soup.find("common-posting-requirements").h3)
+
+requirements = soup.find_all("common-posting-requirements")
+
+# mandatory requirements
+mandatory_req = set()
+for item in requirements[0]:
+    for button in item.find_all("button"):
+        mandatory_req.add(button.text)
+
+# nice to have
+nice_to_have_req = set()
+for item in requirements[1]:
+    for button in item.find_all("button"):
+        nice_to_have_req.add(button.text)
+
+print(f"Mandatory requirements: {mandatory_req}")
+print(f"Nice to have: {nice_to_have_req}")
