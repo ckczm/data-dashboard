4c3b4d692b025bc93d868c2debd1a960c4e89786
ckczm <lukas.zywicki@gmail.com>
2022-03-06 18:30:42 +0100

    ADD: setup postgres during starting airflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# SETUP POSTGRES
+psql postgresql://postgres:postgres@10.108.27.13:8504 -U postgres -f data-dashboard/src/db/nofluff.sql
+echo -e "Nofluffdata database was created."
+
+# SETUP AIRFLOW
 airflow db upgrade
 airflow users create -e admin@ey.com -f Airflow -l Admin -r Admin -u admin -p password
 airflow db init
